908f40322158de1709ef228f73c14b35d5849063
rektdeckard <friedtm@gmail.com>
2021-11-28 01:40:22 -0500

    feat(ui): fancy new mock ui

--- src/components/TestCases/TestCases.styles.tsx
+++ src/components/TestCases/TestCases.styles.tsx
@@ -1,82 +1,155 @@
 import styled from "styled-components";
 
-export const TestCaseContainer = styled.div`
-  color: #35313d;
+export const UIContainer = styled.aside`
+  position: sticky;
+  top: 112px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #efeeef;
+  border-radius: 8px;
+  height: 484px;
+  overflow-y: hidden;
 `;
 
-export const IconButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: 64px 56px 48px 32px;
-  gap: 48px;
+export const UIHeader = styled.div`
+  display: flex;
   align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  color: #9a989e;
+  background-color: #e7e6e8;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
 `;
 
-export const IconButton = styled.div<{ $size: number, $radius: number; $dark?: boolean; }>`
+export const UIHeaderItems = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+`;
+
+export const UINavBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 20px;
+  color: white;
+  background-color: #35313d;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  z-index: 1;
+`;
+
+export const UIBottomSheet = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  padding: 20px 20px 68px;
+  color: white;
+  background-color: #45414d;
+  border-radius: 16px;
+`;
+
+export const UIBottomSheetHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & h2 {
+    color: currentColor;
+    margin: 0;
+    font-size: 14px;
+  }
+`;
+
+export const UIBottomSheetItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const UIContents = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px 24px;
+`;
+
+export const UIChipsContiner = styled.div`
+  display: flex;
+  gap: 8px;
+  padding: 16px 22px 0;
+  align-items: center;
+  overflow-x: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const UIChip = styled.div<{ $active?: boolean }>`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  padding: 6.5px 14.5px 6.5px 10.5px;
+  box-sizing: border-box;
+  background-color: white;
+  border-radius: 20px;
+  color: ${({ $active }) => ($active ? "#35313D" : "#9a989e")};
+  border: 1.5px solid ${({ $active }) => ($active ? "#35313D" : "transparent")};
+  cursor: pointer;
+
+  &:hover {
+    color: #35313d;
+    border-color: #35313d;
+  }
+`;
+
+export const UICard = styled.div`
+  display: flex;
+  gap: 4px;
+  padding: 12px;
+  background-color: white;
+  border-radius: 6px;
+  border-top-right-radius: 28px;
+`;
+
+export const UICardText = styled.div<{ $width?: number; $withIcon?: boolean }>`
+  margin: ${({ $withIcon }) => ($withIcon ? `0 12px` : `12px`)};
+  background-color: #e7e6e8;
+  border-radius: 4px;
+  width: ${({ $width }) => $width && `${$width}px`};
+  height: 16px;
+`;
+
+export const UICardButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const IconButton = styled.div<{
+  $size: number;
+  $radius: number;
+  $active?: boolean;
+}>`
   display: grid;
   place-items: center;
   height: ${({ $size }) => $size}px;
   width: ${({ $size }) => $size}px;
   border-radius: ${({ $radius }) => $radius}px;
-  background: ${({ $dark }) => ($dark ? "#35313d" : "#EBEAEC")};
-  color: ${({ $dark }) => ($dark ? "#EBEAEC" : "#35313d")};
+  background: #e7e6e8;
+  color: ${({ $active }) => ($active ? "#35313d" : "#9A989E")};
 
   & img {
-    color: ${({ $dark }) => ($dark ? "#EBEAEC" : "#35313d")};
+    color: ${({ $active }) => ($active ? "#35313d" : "#9A989E")};
   }
-`;
-
-export const TextButtonContainer = styled.div`
-  margin: 40px 0;
-  display: flex;
-  align-items: center;
-`;
-
-export const TextButton = styled.span<{ $large?: boolean }>`
-  display: inline-flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 8px;
-  border-radius: ${({ $large }) => ($large ? "80" : "24")}px;
-  padding: ${({ $large }) => $large ? "12px 32px 12px 24px" : "12px 20px 12px 16px"};
-  background: #ebeaec;
-  margin-right: 32px;
-  font-size: ${({ $large }) => ($large ? 24 : 16)}px;
-  font-weight: ${({ $large }) => ($large ? "bold" : "normal")};
-`;
-
-export const TestUIContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  border-radius: 8px;
-  background: #ebeaec;
-`;
-
-export const LargeIconContainer = styled.div`
-  flex: 1;
-  display: grid;
-  place-items: center;
-  padding: 48px 108px;
-`;
-
-export const MockMenu = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 24px;
-  padding: 32px 24px;
-  background: #35313d;
-`;
-
-export const MockMenuItem = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 16px;
-  color: #ebeaec;
-`;
-
-export const MockToolbar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 8px 20px;
 `;
--- src/components/TestCases/TestCases.tsx
+++ src/components/TestCases/TestCases.tsx
@@ -1,6 +1,9 @@
 import React, { useMemo } from "react";
 import { useRecoilValue } from "recoil";
 import {
+  Alarm,
+  WifiMedium,
+  BatteryHigh,
   Folder,
   House,
   MusicNotes,
@@ -10,6 +13,12 @@ import {
   UserCircle,
   IconContext,
   Icon,
+  Chats,
+  X,
+  Planet,
+  Robot,
+  Leaf,
+  Ghost,
 } from "phosphor-react";
 
 import { isIcon } from "../../lib";
@@ -20,16 +29,20 @@ import {
   customPreviewAtom,
 } from "../../state";
 import {
-  TestCaseContainer,
-  IconButtonContainer,
+  UIContainer,
+  UIHeader,
+  UIHeaderItems,
+  UINavBar,
+  UIBottomSheet,
+  UIBottomSheetItem,
+  UIBottomSheetHeader,
+  UIChipsContiner,
+  UIChip,
+  UIContents,
+  UICard,
+  UICardText,
   IconButton,
-  TextButtonContainer,
-  TextButton,
-  TestUIContainer,
-  LargeIconContainer,
-  MockMenu,
-  MockMenuItem,
-  MockToolbar,
+  UICardButtons,
 } from "./TestCases.styles";
 
 const TestCases: React.FC<{}> = () => {
@@ -53,8 +66,9 @@ const TestCases: React.FC<{}> = () => {
         // .replace(/#000000/g, "#EBEAEC")
         // .replace(/#000/g, "#EBEAEC")
         // .replace(/black/g, "#EBEAEC"),
-        .replace(/fill="#?(?!none)(?!transparent)\w+"/g, "fill=\"#EBEAEC\"")
-        .replace(/stroke="#?(?!none)(?!transparent)\w+"/g, "stroke=\"#EBEAEC\""),
+        // .replace(/opacity="\d+\w+"/g, 'opacity="0.2" fill="#EBEAEC"')
+        .replace(/fill="#?(?!none)(?!transparent)\w+"/g, 'fill="#EBEAEC"')
+        .replace(/stroke="#?(?!none)(?!transparent)\w+"/g, 'stroke="#EBEAEC"'),
     [textInput, fileInput, customSource]
   );
 
@@ -63,102 +77,38 @@ const TestCases: React.FC<{}> = () => {
     [svgStringDark]
   );
 
-  const displayIcon =
-    !!customPreviewIcon && !isIcon(customPreviewIcon)
-      ? customPreviewIcon.svgString
-      : svgStringDark;
+  const svgStringMid = useMemo(
+    () => svgStringDark.replace(/#EBEAEC/gi, "#9a989e"),
+    [svgStringDark]
+  );
 
   return (
     <IconContext.Provider
       value={{ size: "1em", weight, color: "currentColor", mirrored: false }}
     >
-      <TestCaseContainer>
-        <IconButtonContainer>
-          <IconButton $size={72} $radius={36} $dark>
-            {isControlPreview ? (
-              <ControlIcon size={48} />
-            ) : svgStringDark ? (
-              <img
-                height={48}
-                width={48}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringDark)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={48} />
-            )}
-          </IconButton>
-          <IconButton $size={56} $radius={36} $dark>
-            {isControlPreview ? (
-              <ControlIcon size={32} />
-            ) : svgStringDark ? (
-              <img
-                height={32}
-                width={32}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringDark)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={32} />
-            )}
-          </IconButton>
-          <IconButton $size={48} $radius={36} $dark>
-            {isControlPreview ? (
-              <ControlIcon size={24} />
-            ) : svgStringDark ? (
-              <img
-                height={24}
-                width={24}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringDark)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={24} />
-            )}
-          </IconButton>
-          <IconButton $size={32} $radius={36} $dark>
+      <UIContainer>
+        <UIHeader>
+          <span>8:01</span>
+          <UIHeaderItems>
             {isControlPreview ? (
               <ControlIcon size={16} />
             ) : svgStringDark ? (
               <img
                 height={16}
                 width={16}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringDark)}`}
+                src={`data:image/svg+xml,${encodeURIComponent(svgStringMid)}`}
                 alt=""
               />
             ) : (
               <Smiley size={16} />
             )}
-          </IconButton>
-          <IconButton $size={72} $radius={36}>
-            {isControlPreview ? (
-              <ControlIcon size={48} />
-            ) : svgStringDark ? (
-              <img
-                height={48}
-                width={48}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={48} />
-            )}
-          </IconButton>
-          <IconButton $size={56} $radius={20}>
-            {isControlPreview ? (
-              <ControlIcon size={32} />
-            ) : svgStringDark ? (
-              <img
-                height={32}
-                width={32}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={32} />
-            )}
-          </IconButton>
-          <IconButton $size={48} $radius={8}>
+            <Alarm size={16} />
+            <WifiMedium size={16} />
+            <BatteryHigh size={16} />
+          </UIHeaderItems>
+        </UIHeader>
+        <UIChipsContiner>
+          <UIChip $active>
             {isControlPreview ? (
               <ControlIcon size={24} />
             ) : svgStringDark ? (
@@ -171,115 +121,105 @@ const TestCases: React.FC<{}> = () => {
             ) : (
               <Smiley size={24} />
             )}
-          </IconButton>
-          <IconButton $size={32} $radius={0}>
-            {isControlPreview ? (
-              <ControlIcon size={16} />
-            ) : svgStringDark ? (
-              <img
-                height={16}
-                width={16}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={16} />
-            )}
-          </IconButton>
-        </IconButtonContainer>
-        <TextButtonContainer>
-          <TextButton>
-            {isControlPreview ? (
-              <ControlIcon size={24} />
-            ) : svgStringDark ? (
-              <img
-                height={24}
-                width={24}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={24} />
-            )}{" "}
-            Button
-          </TextButton>
-          <TextButton $large>
-            {isControlPreview ? (
-              <ControlIcon size={48} />
-            ) : svgStringDark ? (
-              <img
-                height={48}
-                width={48}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={48} />
-            )}{" "}
-            Button
-          </TextButton>
-        </TextButtonContainer>
-        <TestUIContainer>
-          <LargeIconContainer>
-            {isControlPreview ? (
-              <ControlIcon size={144} />
-            ) : svgStringDark ? (
-              <img
-                height={144}
-                width={144}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={144} />
-            )}
-          </LargeIconContainer>
-          <MockMenu>
-            <MockMenuItem>Menu Title</MockMenuItem>
-            <MockMenuItem>
-              <Folder size={24} /> Menu Item 1
-            </MockMenuItem>
-            <MockMenuItem>
-              {isControlPreview ? (
-                <ControlIcon size={24} />
-              ) : svgStringDark ? (
-                <img
-                  height={24}
-                  width={24}
-                  src={`data:image/svg+xml,${encodeURIComponent(
-                    svgStringDark
-                  )}`}
-                  alt=""
-                />
-              ) : (
-                <Smiley size={24} />
-              )}{" "}
-              Menu Item 2
-            </MockMenuItem>
-            <MockMenuItem>
-              <PencilLine size={24} /> Menu Item 3
-            </MockMenuItem>
-          </MockMenu>
-          <MockToolbar>
-            <House size={24} />
-            <Star size={24} />
-            {isControlPreview ? (
-              <ControlIcon size={24} />
-            ) : svgStringDark ? (
-              <img
-                height={24}
-                width={24}
-                src={`data:image/svg+xml,${encodeURIComponent(svgStringLight)}`}
-                alt=""
-              />
-            ) : (
-              <Smiley size={24} />
-            )}
+            <span>Latest</span>
+          </UIChip>
+          <UIChip>
+            <Planet size={24} />
+            <span>Space</span>
+          </UIChip>
+          <UIChip>
             <MusicNotes size={24} />
-            <UserCircle size={24} />
-          </MockToolbar>
-        </TestUIContainer>
-      </TestCaseContainer>
+            <span>Music</span>
+          </UIChip>
+          <UIChip>
+            <Robot size={24} />
+            <span>Robots</span>
+          </UIChip>
+          <UIChip>
+            <Ghost size={24} />
+            <span>Spoopy</span>
+          </UIChip>
+        </UIChipsContiner>
+        <UIContents>
+          <UICard>
+            <div style={{ flex: 1 }}>
+              <UICardText />
+              <UICardText />
+              {/* <UICardText $width={208} /> */}
+              <UICardText />
+              <UICardText $width={88} />
+            </div>
+            <UICardButtons>
+              <IconButton $size={48} $radius={48}>
+                <Leaf size={32} />
+              </IconButton>
+              <IconButton $active $size={48} $radius={48}>
+                {isControlPreview ? (
+                  <ControlIcon size={32} />
+                ) : svgStringDark ? (
+                  <img
+                    height={32}
+                    width={32}
+                    src={`data:image/svg+xml,${encodeURIComponent(
+                      svgStringLight
+                    )}`}
+                    alt=""
+                  />
+                ) : (
+                  <Smiley size={32} />
+                )}
+              </IconButton>
+            </UICardButtons>
+          </UICard>
+        </UIContents>
+        <UINavBar>
+          <House size={24} weight="fill" />
+          <Star size={24} />
+          {isControlPreview ? (
+            <ControlIcon size={24} />
+          ) : svgStringDark ? (
+            <img
+              height={24}
+              width={24}
+              src={`data:image/svg+xml,${encodeURIComponent(svgStringDark)}`}
+              alt=""
+            />
+          ) : (
+            <Smiley size={24} />
+          )}
+          <Chats size={24} />
+          <UserCircle size={24} />
+        </UINavBar>
+        <UIBottomSheet>
+          <UIBottomSheetHeader>
+            <h2>Choose one</h2>
+            <X size={16} weight="bold" />
+          </UIBottomSheetHeader>
+          <UIBottomSheetItem>
+            <Folder size={24} />
+            <span>First item in the list</span>
+          </UIBottomSheetItem>
+          <UIBottomSheetItem>
+            {isControlPreview ? (
+              <ControlIcon size={24} />
+            ) : svgStringDark ? (
+              <img
+                height={24}
+                width={24}
+                src={`data:image/svg+xml,${encodeURIComponent(svgStringDark)}`}
+                alt=""
+              />
+            ) : (
+              <Smiley size={24} />
+            )}
+            <span>A second item</span>
+          </UIBottomSheetItem>
+          <UIBottomSheetItem>
+            <PencilLine size={24} />
+            <span>One more item</span>
+          </UIBottomSheetItem>
+        </UIBottomSheet>
+      </UIContainer>
     </IconContext.Provider>
   );
 };
